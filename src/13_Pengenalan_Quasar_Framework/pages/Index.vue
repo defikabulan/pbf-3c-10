a49ce18aa9820e578cdd483343ba41e096d347a0
defikabulan <devikabulan8@gmail.com>
2021-06-24 11:08:15 +0700

    Laporan Modul 13

---
+++ src/13_Pengenalan_Quasar_Framework/pages/Index.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-page class="flex flex-center">
+    <h3>{{ hello }}</h3>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "PageIndex",
+  data() {
+    return {
+      hello: "Halo, Nama Saya Defika Bulan dengan NIM 1841720098",
+    };
+  },
+};
+</script>
